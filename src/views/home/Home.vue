ad2b87b1bc1e8de73bb44fb718a8e326ab9b30f0
tdw143 <623108651@qq.com>
2021-07-10 13:13:20 +0800

    master

--- src/views/home/Home.vue
+++ src/views/home/Home.vue
@@ -1,153 +1,158 @@
 <template>
   <div id="home">
     <nav-bar class="home-nav"><div slot="center">购物街</div></nav-bar>
-    <home-swiper :banner="banner"></home-swiper>
-    <recommend-view :recommend="recommend"></recommend-view>
-    <feature-view/>
-    <tab-control :titles="['流行','新款','精选']"
-                  class="tab-control"/>
-
-    <ul>
-      <li>list1</li>
-      <li>list2</li>
-      <li>list3</li>
-      <li>list4</li>
-      <li>list5</li>
-      <li>list6</li>
-      <li>list7</li>
-      <li>list8</li>
-      <li>list9</li>
-      <li>list10</li>
-      <li>list11</li>
-      <li>list12</li>
-      <li>list13</li>
-      <li>list14</li>
-      <li>list15</li>
-      <li>list16</li>
-      <li>list17</li>
-      <li>list18</li>
-      <li>list19</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-      <li>list20</li>
-    </ul>
-
-
+    <scroll class="content" 
+            ref="scroll" 
+            :probe-type="3" 
+            @scroll="contentScroll"
+            :pull-up-load="true"
+            @pullingUp="loadMore">
+      <home-swiper :banner="banner"></home-swiper>
+      <recommend-view :recommend="recommend"></recommend-view>
+      <feature-view />
+      <tab-control
+        :titles="['流行', '新款', '精选']"
+        class="tab-control"
+        @tabClick="tabClick"
+      />
+      <good-list :goods="showGoods"></good-list>
+    </scroll>
+    <back-top @click.native="backClick" v-show="isShow"></back-top>
   </div>
-  
 </template>
 
 <script>
-  import HomeSwiper from './childComps/HomeSwiper'
-  import RecommendView from './childComps/RecommendView'
-  import FeatureView from './childComps/FeatureView'
+import HomeSwiper from "./childComps/HomeSwiper";
+import RecommendView from "./childComps/RecommendView";
+import FeatureView from "./childComps/FeatureView";
 
-  import NavBar from "components/common/navbar/NavBar"
-  import TabControl from 'components/content/tabControl/TabControl'
+import NavBar from "components/common/navbar/NavBar";
+import Scroll from "components/common/scroll/Scroll";
+import TabControl from "components/content/tabControl/TabControl";
+import GoodList from "components/content/goods/GoodsList";
 
-  import {getHomeMultidata} from 'network/home'
-  
-  export default {
-    name: "Home",
-    components: {
-      HomeSwiper,
-      RecommendView,
-      FeatureView,
-      NavBar,
-      TabControl
+import BackTop from 'components/content/backTop/BackTop'
+
+import { getHomeMultidata, getHomeGoods } from "network/home";
+
+export default {
+  name: "Home",
+  components: {
+    HomeSwiper,
+    RecommendView,
+    FeatureView,
+    NavBar,
+    TabControl,
+    GoodList,
+    Scroll,
+    BackTop
+  },
+  data() {
+    return {
+      banner: [],
+      recommend: [],
+      goods: {
+        pop: { page: 0, list: [] },
+        new: { page: 0, list: [] },
+        sell: { page: 0, list: [] },
+      },
+      currentType: "pop",
+      isShow: false
+    };
+  },
+  created() {
+    // 1.请求多个数据
+    this.getHomeMultidata();
+    // 2.请求商品数据
+    this.getHomeGoods("pop");
+    this.getHomeGoods("new");
+    this.getHomeGoods("sell");
+  },
+  computed: {
+    showGoods() {
+      return this.goods[this.currentType].list;
     },
-    data() {
-      return {
-        banner: [],
-        recommend: []
+  },
+  methods: {
+    /*
+     * 事件监听相关的方法
+     */
+    tabClick(index) {
+      switch (index) {
+        case 0:
+          this.currentType = "pop";
+          break;
+        case 1:
+          this.currentType = "new";
+          break;
+        case 2:
+          this.currentType = "sell";
+          break;
       }
     },
-    created() {
-      // 1.请求多个数据
-      getHomeMultidata().then(res => {
-        console.log(res)
-        this.banner = res.data.banner.list
-        this.recommend = res.data.recommend.list
-        })
-    }
-  }
+    backClick() {
+      console.log('backClick');
+      this.$refs.scroll.scrollTo(0,0)
+    },
+    contentScroll(position) {
+      this.isShow = (-position.y) > 1000
+    },
+    loadMore() {
+      console.log('----');
+      this.getHomeGoods(this.currentType)
+      this.$refs.scroll.scroll.refresh()
+    },
+    /*
+      网络请求相关的方法
+     */
+    getHomeMultidata() {
+      getHomeMultidata().then((res) => {
+        this.banner = res.data.banner.list;
+        this.recommend = res.data.recommend.list;
+      });
+    },
+    getHomeGoods(type) {
+      const page = this.goods[type].page + 1;
+      getHomeGoods(type, page).then((res) => {
+        this.goods[type].list.push(...res.data.list);
+        this.goods[type].page += 1;
+
+        this.$refs.scroll.finishPullUp()
+      });
+    },
+  },
+};
 </script>
 
 <style scoped>
-  #home {
-    padding-top: 44px;
-  }
-  .home-nav {
-    background-color: var(--color-tint);
-    color: #fff;
+#home {
+  padding-top: 44px;
+  height: 100vh;
+  position: relative;
+}
+.home-nav {
+  background-color: var(--color-tint);
+  color: #fff;
 
-    position: fixed;    
-    top: 0;
-    left: 0;
-    right: 0;
-    z-index: 9;
-  }
+  position: fixed;
+  top: 0;
+  left: 0;
+  right: 0;
+  z-index: 9;
+}
 
-  .tab-control {
-    position: sticky;
-    top: 44px;
-  }
+.tab-control {
+  position: sticky;
+  top: 44px;
+  z-index: 9;
+}
+
+.content {
+  position: absolute;
+  top: 44px;
+  bottom: 49px;
+  left: 0;
+  right: 0;
+}
+
 </style>
